5615ce4c05f3416c5d20164e7d3b232d26248bfb
sim-sal <simone.saladino93@gmail.com>
2023-07-13 19:08:42 +0200

    create migration

---
+++ database/migrations/2023_07_13_161855_create_routes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('azienda', 64);
+            $table->string('stazione_di_partenza', 64);
+            $table->string('stazione_di_arrivo', 64);
+            $table->integer('orario_di_partenza');
+            $table->integer('orario_di_arrivo');
+            $table->string('codice_treno')->unique();
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routes');
+    }
+};
