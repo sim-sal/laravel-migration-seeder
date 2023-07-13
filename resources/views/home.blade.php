e7c9fc91cd29d88fae5c0c55a3752cefc0d8b4fd
sim-sal <simone.saladino93@gmail.com>
2023-07-13 18:09:43 +0200

    start project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main-layout')
+
+@section('pageName')
+    Railway Routes
+@endsection
+
+@section('content')
+    <main>
+        <div class="container">
+            <h1>HELLO HOME!</h1>
+        </div>
+    </main>
+@endsection
