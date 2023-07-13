8c7503ed015622318563146b2e6124533e9e2211
sim-sal <simone.saladino93@gmail.com>
2023-07-13 20:03:18 +0200

    create Seeder

--- database/seeders/RouteTableSeeder.php
+++ database/seeders/RouteTableSeeder.php
@@ -5,17 +5,17 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+use App\Models\Route;
+
+class RouteTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            RouteTableSeeder::class
-        ]);
+        Route::factory()->count(20)->create();
     }
 }
